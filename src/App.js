25fa860254e8321871109cdc3319d4f70a5d92c3
Ahren Pradhan <ahrenpradhan@gmail.com>
2020-11-16 22:51:04 +0530

    nested dropdown

--- src/App.js
+++ src/App.js
@@ -4,40 +4,68 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import './App.css';
 
-function App() {
-	const [anchorEl, setAnchorEl] = React.useState(null);
-
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-	let droplist = ['some action', 'some other action', 'hover me for more action'];
-	return (
-		<div className='App'>
-			<Button aria-controls='simple-menu' aria-haspopup='true' onClick={handleClick}>
-				Open Menu
-			</Button>
-			<Menu id='simple-menu' anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-        {
-          droplist.map((e) => Item(e, setAnchorEl))
-        }
-			</Menu>
-		</div>
-	);
-}
-
+// function CustomList
 function Item(itemName, setAnchorEl) {
 	const handleClose = () => {
 		setAnchorEl(null);
 	};
+	if (Array.isArray(itemName)) {
+		return (
+			<>
+				{CustomDropdown(itemName.splice(1), itemName[0],handleClose,1)}
+			</>
+		);
+	}
 	return (
 		<>
 			<MenuItem onClick={handleClose}>{itemName}</MenuItem>
 		</>
 	);
+}
+function CustomDropdown(list, title,handler, sub) {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+		if (handler) {
+			handler();
+		}
+	};
+	return (
+		<div>
+			{sub ? (
+				<MenuItem onClick={handleClick}>{title}</MenuItem>
+			) : (
+				<Button aria-controls='simple-menu' aria-haspopup='true' onClick={handleClick}>
+					{title || 'Open menu'}
+				</Button>
+			)}
+			<Menu
+				anchorEl={anchorEl}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+				transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+				open={Boolean(anchorEl)}
+				onClose={handleClose}>
+				{list.map((e) => Item(e, setAnchorEl))}
+			</Menu>
+		</div>
+	);
+}
+function App() {
+	let droplist = [
+		'some action',
+		'some other action',
+		[
+			'hover me for more action',
+			'Second Level',
+			['Even More', '3th level', ['another level', '4th level', '4th level', '4th level'], '3th level'],
+			'Second Level',
+			'Second Level',
+		],
+	];
+	return <div className='App'>{CustomDropdown(droplist)}</div>;
 }
 
 export default App;
