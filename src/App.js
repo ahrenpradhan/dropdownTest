15ad1e4db5b4c877c9171003f9e6ab7e26728e58
Ahren Pradhan <ahrenpradhan@gmail.com>
2020-11-17 13:18:05 +0530

    update - scope fix

--- src/App.js
+++ src/App.js
@@ -3,35 +3,39 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import './App.css';
-
+// import OutsideClickHandler from 'react-outside-click-handler';
 // function CustomList
-function Item(itemName, setAnchorEl) {
-	const handleClose = () => {
-		setAnchorEl(null);
+function Item(handleUpdate, itemName, handleClose) {
+	// const [selectedIndex, setSelectedIndex] = React.useState(1);
+	const handleMenuItemClick = (event, index) => {
+		// console.log(index);
+		// console.log(itemName);
+		// console.log('--------------------');
+		handleUpdate(itemName);
+		handleClose();
+		// setSelectedIndex(index);
+		// setAnchorEl(null);
 	};
 	if (Array.isArray(itemName)) {
-		return (
-			<>
-				{CustomDropdown(itemName.splice(1), itemName[0],handleClose,1)}
-			</>
-		);
+		return CustomDropdown(handleUpdate, itemName.splice(1), itemName[0], handleClose, 1);
 	}
-	return (
-		<>
-			<MenuItem onClick={handleClose}>{itemName}</MenuItem>
-		</>
-	);
+	return <MenuItem onClick={handleMenuItemClick}>{itemName}</MenuItem>;
 }
-function CustomDropdown(list, title,handler, sub) {
+
+function CustomDropdown(handleUpdate, list, title, handler, sub) {
 	const [anchorEl, setAnchorEl] = React.useState(null);
+	const newRef = React.useRef();
 	const handleClick = (event) => {
 		setAnchorEl(event.currentTarget);
+		if (title) {
+			handleUpdate(title);
+		}
 	};
 	const handleClose = () => {
-		setAnchorEl(null);
 		if (handler) {
 			handler();
 		}
+		setAnchorEl(null);
 	};
 	return (
 		<div>
@@ -43,17 +47,33 @@ function CustomDropdown(list, title,handler, sub) {
 				</Button>
 			)}
 			<Menu
+				ref={newRef}
 				anchorEl={anchorEl}
 				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
 				transformOrigin={{ vertical: 'top', horizontal: 'left' }}
 				open={Boolean(anchorEl)}
-				onClose={handleClose}>
-				{list.map((e) => Item(e, setAnchorEl))}
-			</Menu>
+				onClose={handleClose}
+				children={list.map((e) => Item(handleUpdate, e, handleClose))}
+			/>
 		</div>
 	);
 }
 function App() {
+	const [state, setState] = React.useState([null]);
+	const handleUpdate = (item) => {
+		if (item) {
+			let temp = state.indexOf(item);
+			let arrtemp = state;
+			if (temp!=-1) {
+				arrtemp = arrtemp.slice(0, temp-1);
+			} else {
+				arrtemp = arrtemp.concat(item);
+			}
+			setState(arrtemp);
+		} else {
+			setState([]);
+		}
+	};
 	let droplist = [
 		'some action',
 		'some other action',
@@ -65,7 +85,17 @@ function App() {
 			'Second Level',
 		],
 	];
-	return <div className='App'>{CustomDropdown(droplist)}</div>;
+	return (
+		<div className='App'>
+			{/* <OutsideClickHandler
+				onOutsideClick={() => {
+					// alert('You clicked outside of this component!!!');
+				}}> */}
+			Selected Option - {state[state.length-1]}
+			{CustomDropdown(handleUpdate, droplist)}
+			{/* </OutsideClickHandler> */}
+		</div>
+	);
 }
 
 export default App;
